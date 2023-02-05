5ffe59e3ac2ba328bac34951a6968adf8fb69612
tamrin2mohammad <124502419+tamrin2mohammad@users.noreply.github.com>
2023-02-05 04:00:01 -0800

    Add files via upload

---
+++ meeting1/tamrin1/tamrin.js
@@ -0,0 +1,17 @@
+function sum(num1, num2) {
+    return num1 + num2
+}
+function sub(num1, num2) {
+    return num1 - num2
+}
+function mul(num1, num2) {
+    return num1 * num2
+}
+function div(num1, num2) {
+    return num1 / num2
+}
+console.log(sum(1,4))
+console.log(sub(6,8))
+console.log(mul(4,5))
+console.log(div(20,4))
+
